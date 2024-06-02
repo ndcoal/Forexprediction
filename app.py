31f320bd08a526f68c9bc34df91e041293427608
Ndubisi Micheal Uzoegbu <70382601+ndcoal@users.noreply.github.com>
2024-06-02 16:46:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,15 @@ def predict():
     user_input = request.json
     user_input_df = pd.DataFrame(user_input)
     # Forecast price
-    lin_pred, rf_pred = forecast_price(user_input_df)
+    lin_pred = forecast_price(user_input_df)
     numblen = len(lin_pred)
     # Create prediction results
     pr_results = {
         'Date': user_input_df.index[-numblen:].strftime('%Y-%m-%d %H:%M:%S').tolist(),
-        'lin_pred': (lin_pred + 0.0014).tolist(),
-        'rf_pred': (rf_pred + 0.0014).tolist()
+        'lin_pred': (lin_pred + 0.0014).tolist()
     }
     
     return jsonify(pr_results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=3010, debug=True)
